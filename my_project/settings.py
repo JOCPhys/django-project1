adf3eb4e1c62f0dfe34da296d6313f4a41b91359
Joanna O'Connor <jtoconnor75@gmail.com>
2024-12-19 14:29:12 +0000

    true - false change settings

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3un&*#1qn5vlm7*_%%+@&%)6jx(inz))mk-#oqm2_**ef1t^s-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-nielmc-django-project-0kylrta3cs.us2.codeanyapp.com', 'localhost', '.herokuapp.com']
 
